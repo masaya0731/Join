506f2a8877bc0775a63de287811fb28e41aaf628
masaya0731 <masaya0806543@gmail.com>
2022-04-02 23:59:59 +0900

    [Update] 投稿周り、管理者作成

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,8 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
+  namespace :admin do
+    root to: 'homes#top'
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(email: ENV['ADMIN_ADRESS'], password: ENV['ADMIN_PASSWORD'],
+                  password_confirmation: 'password')
